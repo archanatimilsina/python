2a67812eef0dfb3363792133b4fc7651a44ba7d0
archanatimilsina <archanatimilsina88@gmail.com>
2025-12-08 20:55:02 +0545

    practice set2

--- sets/set.py
+++ sets/set.py
@@ -64,5 +64,78 @@ set1={"Apple","Banana","strawberry","watermeleon","mango"}
 # s.clear()
 # print(s) #output: set()
  
+#set operation
+# union(*others)  Returns a new set containing items from all sets.
+# Equivalent to |
+# a = {1, 2}
+# b = {2, 3}
+# print(a.union(b))        # {1, 2, 3}
+# print(a|b)   # {1, 2, 3}
 
- 
+
+# intersection(*others) Common elements only.
+# Equivalent to &.
+# a = {1, 2, 3}
+# b = {2, 3, 4}
+# print(a.intersection(b) )   # {2, 3}
+# print(a&b)   # {2, 3}
+
+
+# difference(*others) Elements in the first set but not in the others.
+# Equivalent to -.
+# a = {1, 2, 3}
+# b = {2, 3}
+# print(a.difference(b)) # {1}
+# print(a-b) # {1}
+
+
+# symmetric_difference(other) Elements that are in either set, but not in both.
+# Equivalent to ^.
+# a = {1, 2}
+# b = {2, 3}
+# print(a.symmetric_difference(b))     # {1, 3}
+# print(a^b)     # {1, 3}
+
+
+# update()
+# Explained above (like union but modifies).
+# intersection_update(*others)
+# Keeps only the items that are also in the other sets.
+# a = {1, 2, 3}
+# b = {2, 3}
+#print(a.intersection_update(b) )  # a becomes {2, 3}
+
+# difference_update(*others)
+# Removes elements found in the other sets.
+# a = {1, 2, 3}
+# b = {2}
+#print(a.difference_update(b))      # {1, 3}
+
+
+# symmetric_difference_update(other)
+# Replaces the set with the symmetric difference.
+# a = {1, 2}
+# b = {2, 3}
+# print(a.symmetric_difference_update(b))   # a becomes {1, 3}
+
+
+# Relationship-checking methods
+# These don’t modify anything. They answer structural questions.
+
+# issubset(other)
+# True if every element of one set appears in the other.
+# print({1, 2}.issubset({1, 2, 3}))    # True
+
+# issuperset(other)
+# True if the set contains all elements of the other.
+#print({1, 2, 3}.issuperset({1, 2}))   # True
+
+# isdisjoint(other)
+# True if the two sets share no elements.
+#print({1, 2}.isdisjoint({3, 4}))    # True
+
+
+# add, update, remove, discard, pop, clear, union, intersection, difference, symmetric_difference, 
+# intersection_update, difference_update, symmetric_difference_update, issubset, issuperset, isdisjoint
+
+
