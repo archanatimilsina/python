ad3e0ba7fae195f3652735608d3c0a526f224719
archanatimilsina <archanatimilsina88@gmail.com>
2025-12-09 14:38:41 +0545

    practice if..else and pass statement

---
+++ if_else/ifelse.py
@@ -0,0 +1,63 @@
+#if condition
+# temperature= 67
+# if(temperature > 45):
+#     print("Temperature is fine!!!")
+#output: Temperature is fine!!!
+
+# #if....elif condition
+# temperature= 35
+# if(temperature > 45):
+#     print("Temperature is greater than 45 degree!!!")
+# elif temperature <38:
+#     print("temperature is lesser than 38 degree!!!")
+ #output: temperature is lesser than 38 degree!!!
+
+
+# #if...elif...else condition
+# temperature= 40
+# if(temperature > 45):
+#      print("Temperature is greater than 45 degree!!!")
+# elif temperature <38:
+#     print("temperature is lesser than 38 degree!!!")
+# else:
+#     print("temperature is okay!!!")
+#output: temperature is okay!!!
+
+# #if condition : statement
+# temperature= 35
+# if temperature == 35 : print("it is 35 degree")
+#output: it is 35 degree
+
+# # statement if condition else statement
+# temperature= 78
+# print("temperature is lesser than 35") if temperature<35 else print("temperature is greater than 35")
+#output: temperature is greater than 35
+
+# # #and or not in condition
+# name= "Archana1"
+# password="Actlikeanut1"
+# if name== "Archana" and password == "Actlikeanut":
+#     print("correct password")
+# elif name=="Archana" or password == "actlikeanut":
+#     print("name is correct but password is not correct")
+# elif not password =="Actlikeanut":
+#     print("password not correct")
+
+
+#nested if 
+# if conditon:
+#     state1
+#     if ....
+# name= "Archana"
+# password="Actlikeanut"
+# if name=="Archana":
+#     if password== "Actlikeanut":
+#         print("Hello you have entered correct password")
+
+
+#pass statement
+# name= "Archana"
+# if name== "Archana":
+#     pass
+# elif name== "Archana1":
+#     print("you are selected")
